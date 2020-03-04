645c51dc53e99267b45e7e50e510d8a2a4cddd34
william <alexander@gmail.com>
2020-03-04 09:15:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Category } from './components/Category'
+// eslint-disable-next-line padded-blocks
+export const App = () => {
+
+  return (
+    <Category />
+  )
+}
